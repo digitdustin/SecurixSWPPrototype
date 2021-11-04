dd9a279c8728aaa17ec21f25dd5cfe2e145ce826
chancethescholar <chanceonyiorah@gmail.com>
2021-11-03 22:38:36 -0400

    Added routes and form page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import Dashboard from './components/Dashboard.js'
+import { Route, BrowserRouter } from "react-router-dom";
+import Dashboard from './components/Dashboard.js';
+import Forms from './components/Forms.js';
+import Header from './components/Header.js';
 
 function App() {
   return (
     <div className="App">
-      <Dashboard />
+    <BrowserRouter>
+      <Header />
+        <main>
+          <Route path='/' component={Dashboard} exact/>
+          <Route path='/forms' component={Forms} exact/>
+        </main>
+    </BrowserRouter>
     </div>
   );
 }
