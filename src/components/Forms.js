7f7a9a26050523ac78aeab9267f12bbe2353587a
Dustin <dustinkarp52@gmail.com>
2021-11-11 22:54:27 -0500

    added multipage forms

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,9 +1,13 @@
 import MainScreen from "./MainScreen.js";
+import { useContext } from "react";
 import '../assets/css/Forms.css';
 import { Button, ContentSwitcher, Switch } from "carbon-components-react";
 import FormHolder from "./FormHolder.js";
+import {RoleContext, roles} from '../contexts/role-context'
 
 const Forms = () => {
+  const {role, toggleRole} = useContext(RoleContext);
+
   return (
     <div className="bx--grid" style={{height: 'calc(100vh - 30px)', paddingTop: 50, paddingBottom: 80, overflow: 'scroll'}}>
             <div className="bx--row ">
@@ -13,13 +17,15 @@ const Forms = () => {
                 </h1>
                 <p style={{marginBottom: 20}}>View, fill out, and download all Atlas forms.</p>
                 <div style={{backgroundColor: 'white', padding : 20, marginBottom: 20}}>
+                  {role === roles.admin ? 
                   <ContentSwitcher light style={{marginBottom: 20}}>
                     <Switch name="allforms" text="All Forms (24)"></Switch>
                     <Switch name="action" text="Awaiting Action (1)"></Switch>
                   </ContentSwitcher>
+                  : <h4 style={{marginBottom: 20}}>All Forms (24)</h4>}
                   <FormHolder />
                 </div>
-                <p>Don't see what you need? <a>Request a form.</a></p>
+                {role === roles.admin ? <></> : <p>Don't see what you need? <a>Request a form.</a></p>}
             </div>
         </div>
     </div>
